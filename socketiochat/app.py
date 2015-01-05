2711e8e1832189d95fdb5a8989129005bcf11ac7
shinriyo <shinriyo@gmail.comm>
2015-01-05 17:34:28 +0900

    little merge from flaskr

--- socketiochat/app.py
+++ socketiochat/app.py
@@ -35,6 +35,28 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
 @socketio.on('my event', namespace=app.config['NAMESPACE'])
 def test_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
